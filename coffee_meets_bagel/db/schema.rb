5a38c7a2fe4c63862f2e096b4f841ff49e675c60
name": null <email": null>
2015-10-25 13:31:37 -0700

    Add photo model

--- coffee_meets_bagel/db/schema.rb
+++ coffee_meets_bagel/db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025201101) do
+ActiveRecord::Schema.define(version: 20151025202518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
