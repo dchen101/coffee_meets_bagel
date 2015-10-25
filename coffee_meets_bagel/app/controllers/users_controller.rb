b636cb83ffb6cc0cef22f9f8a3e7071d7ef0f382
name": null <email": null>
2015-10-25 15:08:56 -0700

    Add user login

---
+++ coffee_meets_bagel/app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(username: params[:username], password: params[:password])
+    if user.save
+      session[:username] = user.username
+      redirect_to user
+    else
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @user= User.find(params[:id])
+  end
+
+end
