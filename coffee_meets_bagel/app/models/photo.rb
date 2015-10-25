101bbb3648f72ead25b8a189987291f3233bd7f0
name": null <email": null>
2015-10-25 13:57:42 -0700

    Add like model/migration

--- coffee_meets_bagel/app/models/photo.rb
+++ coffee_meets_bagel/app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ActiveRecord::Base
   belongs_to :user
+  has_many :likes
   has_attached_file :image, styles: {medium: "300x300>"},
   content_type: /\Aimage\/.*\z/
 end
--- coffee_meets_bagel/db/schema.rb
+++ coffee_meets_bagel/db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025204132) do
+ActiveRecord::Schema.define(version: 20151025204922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "photo_id"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.integer  "user_id"
