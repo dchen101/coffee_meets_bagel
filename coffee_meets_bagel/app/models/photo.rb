2629d033fa718c966b46e6da5b878f2bd8b43532
name": null <email": null>
2015-10-25 14:10:40 -0700

    Add tags model/migration

--- coffee_meets_bagel/app/models/photo.rb
+++ coffee_meets_bagel/app/models/photo.rb
@@ -2,6 +2,7 @@ class Photo < ActiveRecord::Base
   belongs_to :user
   has_many :likes
   has_many :comments
+  has_many :tags
   has_attached_file :image, styles: {medium: "300x300>"},
   content_type: /\Aimage\/.*\z/
 end
--- coffee_meets_bagel/db/schema.rb
+++ coffee_meets_bagel/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025205843) do
+ActiveRecord::Schema.define(version: 20151025210529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20151025205843) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "tagname"
+    t.integer  "photo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
