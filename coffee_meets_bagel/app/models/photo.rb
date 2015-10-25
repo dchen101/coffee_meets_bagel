34fc509d14ca55bfbc648a2b7c8c2c27e1d2506d
name": null <email": null>
2015-10-25 14:04:27 -0700

    Add likes model/migration

--- coffee_meets_bagel/app/models/photo.rb
+++ coffee_meets_bagel/app/models/photo.rb
@@ -1,6 +1,7 @@
 class Photo < ActiveRecord::Base
   belongs_to :user
   has_many :likes
+  has_many :comments
   has_attached_file :image, styles: {medium: "300x300>"},
   content_type: /\Aimage\/.*\z/
 end
--- coffee_meets_bagel/app/models/user.rb
+++ coffee_meets_bagel/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :photos
   has_many :likes
+  has_many :comments
   has_secure_password
 end
